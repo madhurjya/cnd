5329665659d7567311a31c80aa82dcd32faecb31
Subhajit Das <subhajitdas86@gmail.com>
2018-04-07 16:45:38 +0530

    Cors Support

--- src/service/app.js
+++ src/service/app.js
@@ -3,9 +3,11 @@ import middlewareLogging from './middleware/middleware-logger';
 import middlewarePassport from './middleware/middleware-passport';
 import middlewareRequestParser from './middleware/middleware-request-parser';
 
+import CorsConfig from '../../config/development/cors.json';
 import SecurityConfig from '../../config/development/security.json';
+import middlewareCors from './middleware/middleware-cors';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 export default function (
@@ -17,6 +19,7 @@ export default function (
     middlewareLogging(app, logger);
     middlewareRequestParser(app);
     middlewarePassport(app, userRepository, SecurityConfig);
+    middlewareCors(app, CorsConfig);
 
     const apiRouter = express.Router();
     apiRouter.use('/serviceDefinitions', serviceDefinitionRouter.Router);
